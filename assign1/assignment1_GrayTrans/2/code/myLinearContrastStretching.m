06761411d3e4178ab967c96b41c029e31886133e
sanghviyashiitb <sanghviyash95@gmail.com>
2016-08-09 02:36:31 +0530

    Addition of Report for Q2 and Some Images

--- assign1/assignment1_GrayTrans/2/code/myLinearContrastStretching.m
+++ assign1/assignment1_GrayTrans/2/code/myLinearContrastStretching.m
@@ -1,7 +1,8 @@
 function [outputImage] = myLinearContrastStretching(inputImage)
-	outputImage = zeros(sizeX, sizeY, 'uint8');
-	minVal = min(min(inputImage));
-	maxVal = max(max(inputImage));
+	[sizeX, sizeY] = size(inputImage);
+    outputImage = zeros(sizeX, sizeY, 'uint8');
+	minVal = min(min(inputImage))
+	maxVal = max(max(inputImage))
 
 	f = @(x) uint8(255.0 .* double(x - minVal) ./ double(maxVal - minVal));
 
--- assign1/assignment1_GrayTrans/2/code/myMainScript.m
+++ assign1/assignment1_GrayTrans/2/code/myMainScript.m
@@ -2,28 +2,33 @@
 tic;
 %% Your code here
 im_barbara = imread('../data/barbara.png');
-out_barbara = myHE(im_barbara); % Generates  global enhanced image after histogram equalization
-% out_barbara_AHE = myAHE(im_barbara, 64); % Generates  global enhanced image after histogram equalization
-% out_barbara_contractstretching = myLinearContrastStretching(im_barbara);
-out_barbara_CLAHE = myCLAHE(im_barbara, 64, 0.5); % Generates  global enhanced image after histogram equalization
-myShowImage(im_barbara);
+%out_barbara = myHE(im_barbara); % Generates  global enhanced image after histogram equalization
+out_barbara_CLAHE = myCLAHE(im_barbara, 64,0.005); % Generates  global enhanced image after histogram equalization
+%out_barbara_contractstretching = myLinearContrastStretching(im_barbara);
+%out_barbara_CLAHE = myCLAHE(im_barbara, 64, 0.5); % Generates  global enhanced image after histogram equalization
+%myShowImage(im_barbara);
 % myShowImage(out_barbara);
 % myShowImage(out_barbara_AHE);
-% myShowImage(out_barbara_contractstretching);
-myShowImage(out_barbara_CLAHE);
+%myShowImage(out_barbara_contractstretching);
+%myShowImage(out_barbara_CLAHE);
 
-% im_canyon = imread('../data/canyon.png');
-% im_canyon_r = im_canyon(:,:,1);
+ %im_canyon = imread('../data/canyon.png');
+%im_canyon_r = im_canyon(:,:,1);
 % im_canyon_g = im_canyon(:,:,2);
 % im_canyon_b = im_canyon(:,:,3);
-% out_canyon = cat(3,myHE(im_canyon_r),myHE(im_canyon_g),myHE(im_canyon_b)); % Generates global enhanced image after histogram equalization
+% out_canyon = cat(3,myLinearContrastStretching(im_canyon_r),myLinearContrastStretching(im_canyon_g),myLinearContrastStretching(im_canyon_b)); % Generates global enhanced image after histogram equalization
 % out_canyon_AHE = cat(3,myAHE(im_canyon_r,36),myAHE(im_canyon_g,36),myAHE(im_canyon_b,36));
 % myShowImage(im_canyon);
 % myShowImage(out_canyon);
 % myShowImage(out_canyon_AHE);
 
-% im_TEM = imread('../data/TEM.png');
-% out_TEM = myHE(im_TEM); % Generates enhanced image after global histogram equalization
-% myShowImage(im_TEM);
-% myShowImage(out_TEM);
+ im_TEM = imread('../data/TEM.png');
+ %out_TEM_36 = myAHE(im_TEM,36); % Generates enhanced image after global histogram equalization
+ %out_TEM_64 = myAHE(im_TEM,64); % Generates enhanced image after global histogram equalization
+ %out_TEM_100 = myAHE(im_TEM,100); % Generates enhanced image after global histogram equalization
+
+ myShowImage(im_barbara);
+ myShowImage(out_barbara_CLAHE);
+% myShowImage(out_TEM_64);
+% myShowImage(out_TEM_100);
 toc;
