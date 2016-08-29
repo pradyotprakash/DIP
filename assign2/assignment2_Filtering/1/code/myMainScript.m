1ac3b4bad7157dee2aeaf2d9cf5e4daae76d5cb7
Pradyot Prakash <pp2105@gmail.com>
2016-08-29 19:19:23 +0530

    part 1 and part 2 done

--- assign2/assignment2_Filtering/1/code/myMainScript.m
+++ assign2/assignment2_Filtering/1/code/myMainScript.m
@@ -5,10 +5,10 @@ tic;
 lion_struct = load('../data/lionCrop');
 lion = lion_struct.imageOrig;
 enhanced_lion = myUnsharpMasking(lion, 12, 2, 4);
-plotAndSave(lion, enhanced_lion, 'lion', 1);
+plotAndSave(lion, enhanced_lion, 'lion_enhanced', 1);
 
 moon_struct = load('../data/superMoonCrop');
 moon = moon_struct.imageOrig;
 enhanced_moon = myUnsharpMasking(moon, 12, 1.5, 1);
-plotAndSave(moon, enhanced_moon, 'moon', 2);
+plotAndSave(moon, enhanced_moon, 'moon_enhanced', 2);
 toc;
--- assign2/assignment2_Filtering/2/code/myMainScript.m
+++ assign2/assignment2_Filtering/2/code/myMainScript.m
@@ -1,5 +1,4 @@
 %% MyMainScript
-clc;
 tic;
 %% Your code here
 W = 5; % should always be odd, can't be even
@@ -7,44 +6,33 @@ W = 5; % should always be odd, can't be even
 barbara_struct = load('../data/barbara');
 barbara = barbara_struct.imageOrig;
 
-optS = 1;
-optR = 1;
-mind = inf;
-for sigmaS = 1:0.1:3
-	for sigmaR = 8:0.1:10
-		fprintf('%d %d\n', sigmaS, sigmaR);
-		[enhanced, noisy, err] = myBilateralFiltering(barbara, W, sigmaS, sigmaR);
-		% plotAndSave(barbara, noisy, enhanced, 'optimal', 1);
-		% fprintf('optimal: dist: %d\n', err);
-		if err < mind
-			mind = err;
-			optS = sigmaS;
-			optR = sigmaR;
-		end
-	end
-end
+sigmaS = 1.5;
+sigmaR = 9.7;
+[enhanced, noisy, err] = myBilateralFiltering(barbara, W, sigmaS, sigmaR);
+plotAndSave(barbara, noisy, enhanced, 'optimal', 1);
+fprintf('optimal: dist: %d\n', err);
 
-% [enhanced, noisy, err] = myBilateralFiltering(barbara, W, 0.9*sigmaS, sigmaR);
-% plotAndSave(barbara, noisy, enhanced, '9space', 2);
-% fprintf('0.9 space: dist: %d\n', err);
+[enhanced, noisy, err] = myBilateralFiltering(barbara, W, 0.9*sigmaS, sigmaR);
+plotAndSave(barbara, noisy, enhanced, '9space', 2);
+fprintf('0.9 space: dist: %d\n', err);
 
-% [enhanced, noisy, err] = myBilateralFiltering(barbara, W, 1.1*sigmaS, sigmaR);
-% plotAndSave(barbara, noisy, enhanced, '11space', 3);
-% fprintf('1.1 space: dist: %d\n', err);
+[enhanced, noisy, err] = myBilateralFiltering(barbara, W, 1.1*sigmaS, sigmaR);
+plotAndSave(barbara, noisy, enhanced, '11space', 3);
+fprintf('1.1 space: dist: %d\n', err);
 
-% [enhanced, noisy, err] = myBilateralFiltering(barbara, W, sigmaS, 0.9*sigmaR);
-% plotAndSave(barbara, noisy, enhanced, '9range', 4);
-% fprintf('0.9 range: dist: %d\n', err);
+[enhanced, noisy, err] = myBilateralFiltering(barbara, W, sigmaS, 0.9*sigmaR);
+plotAndSave(barbara, noisy, enhanced, '9range', 4);
+fprintf('0.9 range: dist: %d\n', err);
 
-% [enhanced, noisy, err] = myBilateralFiltering(barbara, W, sigmaS, 1.1*sigmaR);
-% plotAndSave(barbara, noisy, enhanced, '11range', 5);
-% fprintf('1.1 range: dist: %d\n', err);
+[enhanced, noisy, err] = myBilateralFiltering(barbara, W, sigmaS, 1.1*sigmaR);
+plotAndSave(barbara, noisy, enhanced, '11range', 5);
+fprintf('1.1 range: dist: %d\n', err);
 
-% maskS = fspecial('gaussian', [W, W], sigmaS);
-% figure(10000);
-% v = myLinearContrastStretching(maskS);
-% imshow(v, []);
-% axis on;
-% title('gaussian along space');
-% saveas(10000, 'space_gaussian.png');
+maskS = fspecial('gaussian', [W, W], sigmaS);
+figure(10000);
+v = myLinearContrastStretching(maskS);
+imshow(v, []);
+axis on;
+title('gaussian along space');
+saveas(10000, 'space_gaussian.png');
 toc;
