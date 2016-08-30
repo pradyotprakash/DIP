a479a171d7f00a836403e1f1f1a20fcb65559705
sanghviyashiitb <sanghviyash95@gmail.com>
2016-08-30 21:10:28 +0530

    Added Reports and added Zip Files for submission

--- assign2/assignment2_Filtering/1/code/myMainScript.m
+++ assign2/assignment2_Filtering/1/code/myMainScript.m
@@ -4,11 +4,11 @@ tic;
 %% Your code here
 lion_struct = load('../data/lionCrop');
 lion = lion_struct.imageOrig;
-enhanced_lion = myUnsharpMasking(lion, 12, 2, 4);
+enhanced_lion = myUnsharpMasking(lion, 9, 0.6, 3.1);
 plotAndSave(lion, enhanced_lion, 'lion_enhanced', 1);
 
 moon_struct = load('../data/superMoonCrop');
 moon = moon_struct.imageOrig;
-enhanced_moon = myUnsharpMasking(moon, 12, 1.5, 1);
+enhanced_moon = myUnsharpMasking(moon, 9, 0.5, 8);
 plotAndSave(moon, enhanced_moon, 'moon_enhanced', 2);
 toc;
