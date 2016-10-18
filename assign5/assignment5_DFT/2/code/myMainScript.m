dbd34586101d32292f106c8fe10053e46ea88747
Pradyot Prakash <pp2105@gmail.com>
2016-10-19 03:45:49 +0530

    q3 done

--- assign5/assignment5_DFT/2/code/myMainScript.m
+++ assign5/assignment5_DFT/2/code/myMainScript.m
@@ -2,20 +2,24 @@ tic;
 
 load '../data/image_low_frequency_noise';
 
-F2 = fftshift(fft2(Z, 256, 256));
-F3 = F2;
-F3(115:125, 120:125) = 0;
-F3(135:145, 133:138) = 0;
+Y = zeros(512, 512);
+Y(129:384, 129:384) = Z;
 
-I3 = abs(ifft2(ifftshift(F3)));
+F2 = fftshift(fft2(Y));
+F3 = F2;
+F3(234:250, 238:255) = 0;
+F3(270:290, 260:275) = 0;
+
+I3 = real(ifft2(ifftshift(F3)));
+I3 = I3(129:384, 129:384);
 
 figure;
 subplot(2, 2, 1);
-imshow(log(abs(F2)), []);
+imshow(log(1 + abs(F2)), []);
 title('Log DFT');
 
 subplot(2, 2, 2);
-imshow(log(abs(F3)), []);
+imshow(log(1 + abs(F3)), []);
 title('Extra frequency components removed');
 
 subplot(2, 2, 3);
@@ -25,7 +29,5 @@ title('Original noisy image');
 subplot(2, 2, 4);
 imshow(I3, []);
 title('Restored image');
-
-
 
 toc;
--- assign5/assignment5_DFT/3/code/myMainScript.m
+++ assign5/assignment5_DFT/3/code/myMainScript.m
@@ -1,6 +1,39 @@
-%% MyMainScript
-
 tic;
-%% Your code here
+
+I1 = imread('../data/barbara256.png');
+I2 = zeros(512, 512);
+I2(129:384, 129:384) = I1;
+
+F2 = fftshift(fft2(I2));
+
+for D = [40, 80]
+	center = [256; 256];
+
+	[X, Y] = meshgrid(1:512);
+	C = sqrt((X - center(1)) .^ 2 + (Y - center(2)) .^ 2) <= D;
+
+	F3 = F2 .* C;
+	T = real(ifft2(ifftshift(F3)));
+	T = T(129:384, 129:384);
+	fig = figure;
+	subplot(1, 2, 1);
+	imshow(log(1 + abs(F3)), []);
+	subplot(1, 2, 2);
+	imshow(T, []);
+	saveas(fig, strcat('../images/idealLowPass_', num2str(D), '.png'));
+end
+
+for sig = [40, 80]
+	h = fspecial('gaussian', [512 512], sig);
+	F3 = F2 .* h;
+	T = real(ifft2(ifftshift(F3)));
+	T = T(129:384, 129:384);
+	fig = figure;
+	subplot(1, 2, 1);
+	imshow(log(1 + abs(F3)), []);
+	subplot(1, 2, 2);
+	imshow(T, []);
+	saveas(fig, strcat('../images/gaussianLowPass_', num2str(sig), '.png'));
+end
 
 toc;
